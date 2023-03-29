2cde4234d872e2a8a31ad80581221b01b8fc9120
Priyanshu Sharma <85369616+iamprynshu@users.noreply.github.com>
2023-03-29 23:43:12 +0530

    Added solution - LeetHub

--- Edit Distance - GFG/edit-distance.cpp
+++ Edit Distance - GFG/edit-distance.cpp
@@ -9,17 +9,19 @@ class Solution {
         int n = s.size(); 
         int m = t.size();
         
-        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
-        for(int j=0; j<=m; j++) dp[0][j] = j;
-        for(int i=0; i<=n; i++) dp[i][0] = i;
+        vector<int> prev(m+1, 0), cur(m+1, 0);
+        for(int j=0; j<=m; j++) prev[j] = j;
+        
         
         for(int i=1; i<=n; i++){
+            cur[0] = i;
             for(int j=1; j<=m; j++){
-                if(s[i-1] == t[j-1]) dp[i][j] = dp[i-1][j-1];
-                else dp[i][j] = 1 + min(dp[i][j-1], min(dp[i-1][j], dp[i-1][j-1]));
+                if(s[i-1] == t[j-1]) cur[j] = prev[j-1];
+                else cur[j] = 1 + min(cur[j-1], min(prev[j], prev[j-1]));
             }
+            prev = cur;
         }
-        return dp[n][m];
+        return prev[m];
     }
 };
 
